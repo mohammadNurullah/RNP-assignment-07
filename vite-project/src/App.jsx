c5244a0ad6f23acb6e7a350bcfe53f297482b2a3
mohammadNurullah <nurullahsarkar963@gmail.com>
2023-07-18 01:45:40 +0600

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,63 @@
+// import ExternalUser from "./component/externalUser/ExternalUser";
+import "bootstrap/dist/css/bootstrap.css";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <ExternalUser />
+//     </div>
+//   );
+// };
+
+// export default App;
+//////////////////////////////////
+///////////////
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, { description: newTask, completed: false }]);
+      setNewTask("");
+    }
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <form onSubmit={handleAddTask}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleInputChange}
+          placeholder="Enter a new task"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task.description}
+            <button onClick={() => handleRemoveTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
